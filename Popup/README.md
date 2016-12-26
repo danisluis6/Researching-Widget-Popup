2325ed375193870178f89907843d2f90aa290cfa
danisluis6 <vuongluis@hotmail.com>
2016-12-26 15:32:56 +0700

    ok

--- Popup/README.md
+++ Popup/README.md
@@ -10,7 +10,21 @@
   - [Version1](#version1)
   - [RelationShip](#relationship)
 
+COLOR: https://material.io/guidelines/style/color.html#
+
 ##Version1
 <p align="center">
   <img src="https://github.com/danisluis6/Researching-Widget-Android/blob/version1/AlertDialog/version1_1.png">
+</p>
+
+<p align="center">
+  <img src="https://github.com/danisluis6/Researching-Widget-Android/blob/version1/AlertDialog/version1_2.png">
+</p>
+
+<p align="center">
+  <img src="https://github.com/danisluis6/Researching-Widget-Android/blob/version1/AlertDialog/version1_3.png">
+</p>
+
+<p align="center">
+  <img src="https://github.com/danisluis6/Researching-Widget-Android/blob/version1/AlertDialog/version1_4.png">
 </p>
--- Popup/app/src/main/java/com/example/enclaveit/popup/MainActivity.java
+++ Popup/app/src/main/java/com/example/enclaveit/popup/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.enclaveit.popup;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.PopupMenu;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity implements PopupMenu.OnMenuItemClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        this.findViewById(R.id.demo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this,view);
+                popupMenu.setOnMenuItemClickListener(MainActivity.this);
+                popupMenu.inflate(R.menu.layout_popup);
+                popupMenu.show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.Philippines:
+                findViewById(R.id.activity_main).setBackgroundResource(R.drawable.a);
+                return true;
+            case R.id.Netherlands:
+                findViewById(R.id.activity_main).setBackgroundResource(R.drawable.b);
+                return true;
+            case R.id.Russia:
+                findViewById(R.id.activity_main).setBackgroundResource(R.drawable.c);
+                return true;
+        }
+        return false;
     }
 }
